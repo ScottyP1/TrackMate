be5adcb8c7df750c54a8cecc11767289d97dd5af
Cody Scott <scotty_p@Codys-MacBook-Pro.local>
2025-03-09 18:27:13 -0400

    Update: Track App

--- src/app/(routes)/TrackMateApp/page.js
+++ src/app/(routes)/TrackMateApp/page.js
@@ -1,40 +1,25 @@
+import { FaApple } from "react-icons/fa";
+
 export default function TrackMateApp() {
     return (
-        <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center p-6">
-            <h1 className="text-4xl font-semibold mb-6 animate-fade-in">
+        <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center p-6 mt-12">
+            <h1 className="text-4xl font-semibold mb-6 animate-fade-in text-center">
                 Download Our Mobile App
             </h1>
-            <p className="text-xl mb-8 max-w-3xl text-center opacity-75 animate-fade-in-delay-1s">
-                Stay up to date and receive updates and messages on the go. Our app keeps you connected with the latest features and notifications. Track your favorite tracks, send and receive messages from other riders, and get notified when someone interacts with your content. Download it today to keep everything at your fingertips!
+            <p className="text-xl mb-8 text-center opacity-75 animate-fade-in-delay-1s">
+                Stay up to date and receive updates and messages on the go. Our app keeps you connected with the latest features and notifications. Track your favorite tracks, send and receive messages from other riders, and get notified when a track from your favorites list post updates. Download it today to keep everything at your fingertips!
             </p>
-            <span>Coming Soon</span>
-            {/* <div className="flex justify-center gap-10 mb-10 animate-fade-in-delay-2s">
-                <div className="w-72 h-96 bg-gradient-to-b from-black/[.5] to-black text-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
-                    <div className="h-1/3 bg-blue-600"></div>
-                    <div className="p-4">
-                        <h3 className="text-xl font-semibold mb-4">Track Your Journey</h3>
-                        <p>Get real-time insights into your progress, favorite tracks, and your activity across the app.</p>
-                    </div>
-                </div>
-                <div className="w-72 h-96 bg-gradient-to-b from-black/[.5] to-black text-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
-                    <div className="h-1/3 bg-green-600"></div>
-                    <div className="p-4">
-                        <h3 className="text-xl font-semibold mb-4">Send and Receive Messages</h3>
-                        <p>Connect with riders and track owners instantly with our messaging system. Get notified when you receive new messages.</p>
-                    </div>
-                </div>
-                <div className="w-72 h-96 bg-gradient-to-b from-black/[.5] to-black text-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
-                    <div className="h-1/3 bg-indigo-600"></div>
-                    <div className="p-4">
-                        <h3 className="text-xl font-semibold mb-4">Track Owner Dashboard</h3>
-                        <p>Track owners can manage their page and get notified when someone likes or comments on their track.</p>
-                    </div>
-                </div>
+            <div className="flex flex-wrap gap-4">
+                <img src='./images/localTracksImg.png' width={400} height={300} className="rounded-xl" />
+                <img src='./images/commentsImg.png' width={400} height={300} className="rounded-xl" />
+                <img src='./images/designImg.png' width={400} height={300} className="rounded-xl" />
+                <img src='./images/messageImg.png' width={400} height={300} className="rounded-xl" />
             </div>
+            <button className="mt-6 bg-white border-2 border-black text-black font-semibold py-8 px-8 rounded-full shadow-lg transition transform hover:scale-105 duration-300 ease-in-out flex items-center gap-3">
+                <FaApple color="black" size={30} />
+                <span>Download On The App Store</span>
+            </button>
 
-            <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-8 rounded-full shadow-lg transition transform hover:scale-105 duration-300 ease-in-out">
-                Download Now
-            </button> */}
         </div>
     );
 }
